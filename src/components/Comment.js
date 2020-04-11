f9e503f61b8079815c4c0caf1467eacd247f20a8
Luan Martins <luanrem@gmail.com>
2020-04-11 18:57:08 -0300

    One comment implemented

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,1 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import './Comment.css'
+
+
+class Comment extends Component {
+  render() {
+    const { id, author, date, content } = this.props.comment;
+
+    return (
+      <div className="post_comments" >
+        <div className="foto">
+          <img src={author.avatar} />
+        </div>
+        <section className="write_content">
+          <p className="comment_content"> {author.name}: {content} </p>
+        </section>
+      </div>
+    );
+  };
+}
+
+export default Comment;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Post.css'
 
+import Comment from './Comment';
+
 import Foto from '../assets/Foto.jpg'
 
-class Post extends Component {
+function Post({ post }) {
 
-  render() {
-    return (
-      <div className="post_session" >
-        <div className="post_content" >
-            <a><img src={Foto}/></a>
-            <h1>Júlio Alcantara</h1>
-            <h3 className="post_date">04 Jun 2019</h3>
-          <div className="person_comment">
-          </div>
-        </div>
+  return (
 
-        <hr className="hr1" />
-
-        <div className="post_comments" >
-          <h1>Post comentários</h1>
-          <h1>Post conteúdo</h1>
-          <h1>Post conteúdo</h1>
-          <h1>Post conteúdo</h1>
-        </div>
+    <div className="post_session" >
+      <div className="post_content" >
+        <a><img src={post.author.avatar} /></a>
+        <strong>
+          <h1>{post.author.name}</h1>
+          <p className="post_date">{post.date}</p>
+        </strong>
       </div>
-    );
-  }
+
+      <hr className="hr1" />
+
+      <Comment 
+        key={post.comments.id}
+        comment={post}
+      />
+
+    </div>
+  );
 }
+
 
 export default Post;
